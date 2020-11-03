e6494361b8c8018cbe51001df2533e3a56cef58b
Asha <94jsasha@gmail.com>
2020-11-03 15:53:41 +0100

    Add exercise 03 Context display Name

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -8,6 +8,7 @@ import {Switch} from '../switch'
 // 📜 https://reactjs.org/docs/context.html#reactcreatecontext
 
 const ToggleContext = React.createContext()
+ToggleContext.displayName = "ToggleContext"
 
 function Toggle(props) {
   const [on, setOn] = React.useState(false)
